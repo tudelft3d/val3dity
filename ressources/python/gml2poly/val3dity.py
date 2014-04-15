b89b46a240c0056270a49dd9ba0d6ef252aee6bf
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-04-15 16:41:45 +0200

    val3dity.py to call the GML parsing (gml2poly) + val3idty

---
+++ ressources/python/gml2poly/val3dity.py
@@ -0,0 +1,139 @@
+import os
+import sys
+import shutil
+import glob
+import subprocess
+from lxml import etree
+from StringIO import StringIO
+import gml2poly
+
+VAL3DITYEXE = '/Users/hugo/projects/val3dity/val3dity'
+TEMPFOLDER  = '/Users/hugo/temp/tmpolys'
+
+dErrors = {
+  100: 'REPEATED_POINTS',
+  110: 'RING_NOT_CLOSED',   
+  120: 'RING_SELF_INTERSECT',
+  200: 'SELF_INTERSECTION',  
+  210: 'NON_PLANAR_SURFACE',                     
+  220: 'INTERIOR_DISCONNECTED',
+  230: 'HOLE_OUTSIDE',
+  240: 'HOLES_ARE_NESTED',                      
+  250: 'ORIENTATION_RINGS_SAME',
+  300: 'NOT_VALID_2_MANIFOLD',
+  301: 'SURFACE_NOT_CLOSED',                     
+  302: 'DANGLING_FACES',                         
+  303: 'FACE_ORIENTATION_INCORRECT_EDGE_USAGE',  
+  304: 'FREE_FACES',                             
+  305: 'SURFACE_SELF_INTERSECTS',                
+  306: 'VERTICES_NOT_USED',                      
+  310: 'SURFACE_NORMALS_WRONG_ORIENTATION',      
+  400: 'SHELLS_FACE_ADJACENT',                   
+  410: 'SHELL_INTERIOR_INTERSECT',               
+  420: 'INNER_SHELL_OUTSIDE_OUTER',              
+  430: 'INTERIOR_OF_SHELL_NOT_CONNECTED', 
+}
+
+
+def main():
+  fin = open(sys.argv[1])
+  construct_polys(fin)
+  validate_polys(fin)
+  shutil.rmtree(TEMPFOLDER)
+
+def construct_polys(fin):
+  print "Extracting the solids from the CityGML file"
+  if not os.path.exists(TEMPFOLDER):
+    os.mkdir(TEMPFOLDER)
+  else:
+    shutil.rmtree(TEMPFOLDER)
+    os.mkdir(TEMPFOLDER)
+  gml2poly.process(fin, TEMPFOLDER) # TODO: snap tolerance?
+  print "done.\n"
+
+
+
+
+def validate_polys(fin):
+  print "Validating each solid"
+  # validate each building/shell
+  os.chdir(TEMPFOLDER)
+  dFiles = {}
+  for f in os.listdir(TEMPFOLDER):
+    if f[-4:] == 'poly':
+      i = (f.split('.poly')[0]).rfind('.')
+      f1 = f[:i]
+      if f1 not in dFiles:
+        dFiles[f1] = [f]
+      else:
+        dFiles[f1].append(f)
+  i = 0
+  print "Number of solids in file:", len(dFiles)
+  invalidsolids = 0
+  xmlsolids = []
+  exampleerrors = []
+  for solidname in dFiles:
+    # check if solid or multisurface in first file
+    t = open(dFiles[solidname][0])
+    t.readline()
+    if t.readline().split()[1] == '0':
+      multisurface = True
+    else:
+      multisurface = False
+    t.close()
+    
+    # validate with val3dity
+    str1 = VAL3DITYEXE + " -xml " +  " ".join(dFiles[solidname])
+    op = subprocess.Popen(str1.split(' '),
+                          stdout=subprocess.PIPE, 
+                          stderr=subprocess.PIPE)
+    R = op.poll()
+    if R:
+       res = op.communicate()
+       raise ValueError(res[1])
+    o =  op.communicate()[0]
+    if o.find('ERROR') != -1:
+      invalidsolids += 1
+      i = o.find('<errorCode>')
+      while (i != -1):
+        if exampleerrors.count(o[i+11:i+14]) == 0:
+          exampleerrors.append(o[i+11:i+14])
+        tmp = o[i+1:].find('<errorCode>')
+        if tmp == -1:
+          i = -1
+        else:
+          i = tmp + i + 1
+      o = '\t<Solid>\n\t\t<id>' + solidname + '</id>\n' + o + '\t</Solid>'
+      xmlsolids.append(o)
+    else: #-- no error detected, WARNING if MultiSurface!
+      if multisurface == True:
+        # print 'WARNING: MultiSurfce is actually a valid solid'
+        s = []
+        s.append("\t\t<ValidatorMessage>")
+        s.append("\t\t\t<type>WARNING</type>")
+        s.append("\t\t\t<explanation>MultiSurfaces form a valid Solid</explanation>")
+        s.append("\t\t</ValidatorMessage>\n")
+        o = "\n".join(s)
+        o = '\t<Solid>\n\t\t<id>' + solidname + '</id>\n' + o + '\t</Solid>'
+        xmlsolids.append(o)
+    # o = '\t<Solid>\n\t\t<id>' + solidname + '</id>\n' + o + '\t</Solid>'
+
+  totalxml = []
+  totalxml.append('<ValidatorContext>')
+  totalxml.append('\t<inputFile>' + fin.name + '</inputFile>')
+  totalxml.append("\n".join(xmlsolids))
+  totalxml.append('</ValidatorContext>')
+  s = fin.name
+  s = s[:-4] + '_report.xml'
+  fout = open(s, 'w')
+  fout.write('\n'.join(totalxml))
+  fout.close()
+  print "Invalid solids: ", invalidsolids
+  print "Errors present:"
+  for each in exampleerrors:
+    print each, dErrors[int(each)]
+  print "Report of the validation:", s
+
+if __name__ == "__main__":
+    main()
+  
