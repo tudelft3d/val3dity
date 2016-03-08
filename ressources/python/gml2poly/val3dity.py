f31f2d7ba8d570190015ab6129245b7d093d0dde
Hugo Ledoux <h.ledoux@tudelft.nl>
2016-03-08 12:22:46 +0100

    copyright 2016 changed everywhere

--- ressources/python/gml2poly/val3dity.py
+++ ressources/python/gml2poly/val3dity.py
@@ -1,4 +1,4 @@
-# val3dity - Copyright (c) 2011-2015, Hugo Ledoux.  All rights reserved.
+# val3dity - Copyright (c) 2011-2016, Hugo Ledoux.  All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
