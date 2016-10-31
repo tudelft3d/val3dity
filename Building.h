216dda0fe7cb64fb2db7f1ee1c0370f3aabdd8cb
Hugo Ledoux <h.ledoux@tudelft.nl>
2016-10-31 11:10:28 +0100

    WIP Buildings

--- Building.h
+++ Building.h
@@ -10,27 +10,7 @@
 #define Building_h
 
 #include "definitions.h"
-
-class Building
-{
-public:
-  Building  ();
-  ~Building ();
-
-  bool          validate(double tol_planarity_d2p, double tol_planarity_normals);
-  bool          is_valid();
-  bool          is_empty();
-  std::string   get_report_xml();
-
-  std::string   get_id();
-  void          set_id(std::string id);
-
-protected:
-  std::string                _id;
-  std::vector<BuildingPart*> _lsBP;
-  std::vector<Primitive*>    _lsPrimitives;
-};
-
+#include "Primitive.h"
 
 class BuildingPart
 {
@@ -48,6 +28,26 @@ public:
 
 protected:
   std::string                _id;
+  std::vector<Primitive*>    _lsPrimitives;
+};
+
+class Building
+{
+public:
+  Building  ();
+  ~Building ();
+  
+  bool          validate(double tol_planarity_d2p, double tol_planarity_normals);
+  bool          is_valid();
+  bool          is_empty();
+  std::string   get_report_xml();
+  
+  std::string   get_id();
+  void          set_id(std::string id);
+  
+protected:
+  std::string                _id;
+  std::vector<BuildingPart*> _lsBP;
   std::vector<Primitive*>    _lsPrimitives;
 };
 
