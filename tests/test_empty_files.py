809cb6e5c0660e232bd92bbba49c2de40edf0021
Balázs Dukai <balazs.dukai@gmail.com>
2017-10-04 18:18:25 +0200

    Add tests on empty primitives

--- tests/test_empty_files.py
+++ tests/test_empty_files.py
@@ -28,8 +28,25 @@ def data_completely_empty(request, data_empty):
             request.param))
     return(file_path)
 
+
+@pytest.fixture(scope="module",
+                params=["empty_primitive.gml",
+                        "empty_primitive.xml",
+                        "empty_primitive.json",
+                        "empty_primitive.obj",
+                        "empty_primitive.off"])
+def data_empty_primitive(request, data_empty):
+    file_path = os.path.abspath(
+        os.path.join(
+            data_empty,
+            request.param))
+    return(file_path)
+
 #----------------------------------------------------------------------- Tests
 def test_completely_empty(validate, data_completely_empty, citymodel):
     error = validate(data_completely_empty, options=citymodel)
     assert(error == [901])
-## 902
+
+def test_empty_primitive(validate, data_empty_primitive, citymodel):
+    error = validate(data_empty_primitive, options=citymodel)
+    assert(error == [902])
