63b84e200cce5f9d1f3c292cf3026554af20534c
Balázs Dukai <balazs.dukai@gmail.com>
2018-02-06 18:16:17 +0100

    Add baseline html report template
    from src/reportoutput.h

---
+++ report_update/val3dityconfig.js
@@ -0,0 +1,356 @@
+var errors = {
+    "101": {
+        "name": "101 – TOO_FEW_POINTS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#too-few-points"
+        }
+    },
+    "102": {
+        "name": "102 – CONSECUTIVE_POINTS_SAME",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#consecutive-points-same"
+        }
+    },
+    "103": {
+        "name": "103 – NOT_CLOSED",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#not-closed"
+        }
+    },
+    "104": {
+        "name": "104 – SELF_INTERSECTION",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#self-intersection"
+        }
+    },
+    "201": {
+        "name": "201 – INTERSECTION_RINGS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#intersection-rings"
+        }
+    },
+    "202": {
+        "name": "202 – DUPLICATED_RINGS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#duplicated-rings"
+        }
+    },
+    "203": {
+        "name": "203 – NON_PLANAR_POLYGON_DISTANCE_PLANE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#non-planar-polygon-distance-plane"
+        }
+    },
+    "204": {
+        "name": "204 – NON_PLANAR_POLYGON_NORMALS_DEVIATION",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#non-planar-polygon-normals-deviation"
+        }
+    },
+    "205": {
+        "name": "205 – INTERIOR_DISCONNECTED",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#interior-disconnected"
+        }
+    },
+    "206": {
+        "name": "206 – INNER_RING_OUTSIDE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#inner-ring-outside"
+        }
+    },
+    "207": {
+        "name": "207 – INNER_RINGS_NESTED",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#inner-rings-nested"
+        }
+    },
+    "208": {
+        "name": "208 – ORIENTATION_RINGS_SAME",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#orientation-rings-same"
+        }
+    },
+    "301": {
+        "name": "301 – TOO_FEW_POLYGONS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#too-few-polygons"
+        }
+    },
+    "302": {
+        "name": "302 – NOT_CLOSED",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#error-302"
+        }
+    },
+    "303": {
+        "name": "303 – NON_MANIFOLD_CASE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#non-manifold-case"
+        }
+    },
+    "305": {
+        "name": "305 – MULTIPLE_CONNECTED_COMPONENTS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#multiple-connected-components"
+        }
+    },
+    "306": {
+        "name": "306 – SELF_INTERSECTION",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#error-306"
+        }
+    },
+    "307": {
+        "name": "307 – POLYGON_WRONG_ORIENTATION",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#polygon-wrong-orientation"
+        }
+    },
+    "401": {
+        "name": "401 – INTERSECTION_SHELLS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#intersection-shells"
+        }
+    },
+    "402": {
+        "name": "402 – DUPLICATED_SHELLS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#duplicated-shells"
+        }
+    },
+    "403": {
+        "name": "403 – INNER_SHELL_OUTSIDE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#inner-shell-outside"
+        }
+    },
+    "404": {
+        "name": "404 – INTERIOR_DISCONNECTED",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#error-404"
+        }
+    },
+    "405": {
+        "name": "405 – WRONG_ORIENTATION_SHELL",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#wrong-orientation-shell"
+        }
+    },
+    "501": {
+        "name": "501 – INTERSECTION_SOLIDS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#intersection-solids"
+        }
+    },
+    "502": {
+        "name": "502 – DUPLICATED_SOLIDS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#duplicated-solids"
+        }
+    },
+    "503": {
+        "name": "503 – DISCONNECTED_SOLIDS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#disconnected-solids"
+        }
+    },
+    "601": {
+        "name": "601 – BUILDINGPARTS_OVERLAP",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#buildingparts-overlap"
+        }
+    },
+    "609": {
+        "name": "609 – CITYOBJECT_HAS_NO_GEOMETRY",
+        "link": {
+            "href": "https://val3dity.readthedocs.io/en/v2/errors/#cityobject-has-no-geometry"
+        }
+    },
+    "901": {
+        "name": "901 – INVALID_INPUT_FILE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#invalid-input-file"
+        }
+    },
+    "902": {
+        "name": "902 – EMPTY_PRIMITIVE",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#empty-primitive"
+        }
+    },
+    "903": {
+        "name": "903 – WRONG_INPUT_PARAMETERS",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#wrong-input-parameters"
+        }
+    },
+    "999": {
+        "name": "999 – UNKNOWN_ERROR",
+        "link": {
+            "href": "http://geovalidation.bk.tudelft.nl/val3dity/docs/errors/#unknown-error"
+        }
+    }
+}
+
+function error_overview() {
+
+  if (report.overview_errors == null) {
+    var h = document.createElement("H3")
+    var t = document.createTextNode("No errors!");
+    h.appendChild(t);
+    document.body.appendChild(h);
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "http://geovalidation.bk.tudelft.nl/val3dity/img/welldone.png");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+    return;
+  }
+  else {
+    var h = document.createElement("H3")
+    var t = document.createTextNode("Overview of errors");
+    h.appendChild(t);
+    document.body.appendChild(h);
+    idx_error_table();
+  }
+}
+
+function idx_error_table(){
+
+    var body = document.body,
+        tbl  = document.createElement('table');
+    tbl.setAttribute('id', "errors");
+
+
+    var tr = tbl.insertRow(0);
+    var headers = ['Error', '# of primitives'];
+    for (var i = 0; i < 2; i++){
+        var h = document.createElement('th');
+        h.appendChild(document.createTextNode(headers[i]));
+        tr.appendChild(h);
+    }
+
+    var nr_errors = report.overview_errors.length;
+    var l_errors = report.overview_errors;
+    var nr_prims = report.overview_errors_no_primitives;
+
+    for(var i = 0; i < nr_errors; i++){
+        var tr = tbl.insertRow();
+        var td0 = tr.insertCell(0);
+        var td1 = tr.insertCell(1);
+        var err = errors[l_errors[i]];
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(err.name);
+        a.appendChild(linkText);
+        a.title = err.name;
+        a.href = err.link.href;
+        a.target = '_blank';
+        td0.appendChild(a);
+        if (nr_prims[i] < 0) {
+            // this happens on input error
+            td1.appendChild(document.createTextNode("\u2014"));
+        } else {
+            td1.appendChild(document.createTextNode(nr_prims[i]));
+        }
+
+        // set colour
+        if (l_errors[i] < 200) {
+            tr.setAttribute('class', 'linear-ring-level');
+        } else if (100 < l_errors[i] && l_errors[i] < 300) {
+            tr.setAttribute('class', 'polygon-level');
+        } else if(200 < l_errors[i] && l_errors[i] < 400) {
+            tr.setAttribute('class', 'shell-level');
+        } else if (300 < l_errors[i] && l_errors[i] < 500) {
+            tr.setAttribute('class', 'solid-level');
+        } else if (400 < l_errors[i] && l_errors[i] < 600) {
+            tr.setAttribute('class', 'solid-interactions-level');
+        } else if (500 < l_errors[i] && l_errors[i] < 700) {
+            tr.setAttribute('class', 'citygml-building');
+        } else if (800 < l_errors[i] && l_errors[i] < 1000) {
+            tr.setAttribute('class', 'others');
+        }
+    }
+    body.appendChild(tbl);
+}
+
+function idx_validation_summary(){
+    var body = document.body,
+        tbl  = document.createElement('table');
+    tbl.setAttribute('id', "summary");
+
+    var tr = tbl.insertRow(0);
+    var headers = [" ", "Total", "Valid", "Invalid"];
+    for (var i = 0; i < 4; i++){
+        var h = document.createElement('th');
+        h.appendChild(document.createTextNode(headers[i]));
+        tr.appendChild(h);
+    }
+
+    if (report.CityObjects != null) {
+        var tr = tbl.insertRow();
+
+        // cell CityObjects
+        var td0 = tr.insertCell(0);
+        var a = document.createElement('a');
+        var linkText = document.createTextNode('CityObjects (click for more details)');
+        a.appendChild(linkText);
+        a.title = "CityObjects";
+        a.href = "CityObjects.html";
+        td0.appendChild(a);
+        // cell Total
+        var td_t = tr.insertCell(1);
+        td_t.appendChild(document.createTextNode(report.total_cityobjects));
+        // cell Valid
+        var td_v = tr.insertCell(2);
+        var CO_valid_pc = Math.round(report.valid_cityobjects / report.total_cityobjects * 100);
+        var v = report.valid_cityobjects + ' (' + CO_valid_pc + '%)';
+        td_v.appendChild(document.createTextNode(v));
+        // cell Invalid
+        var td_i = tr.insertCell(3);
+        var CO_invalid_pc = Math.round(report.invalid_cityobjects / report.total_cityobjects * 100);
+        var i = report.invalid_cityobjects + ' (' + CO_invalid_pc + '%)';
+        td_i.appendChild(document.createTextNode(i));
+    }
+
+    if (report.Primitives != null) {
+        var tr = tbl.insertRow();
+
+        // cell Primitives
+        var td0 = tr.insertCell(0);
+        var a = document.createElement('a');
+        var linkText = document.createTextNode('Primitives');
+        a.appendChild(linkText);
+        a.title = "Primitives";
+        a.href = "Primitives.html";
+        td0.appendChild(a);
+        // cell Total
+        var td_t = tr.insertCell(1);
+        td_t.appendChild(document.createTextNode(report.total_primitives));
+        // cell Valid
+        var td_v = tr.insertCell(2);
+        var p_valid_pc = Math.round(report.valid_primitives / report.total_primitives * 100);
+        var v = report.valid_primitives + ' (' + p_valid_pc + '%)';
+        td_v.appendChild(document.createTextNode(v));
+        // cell Invalid
+        var td_i = tr.insertCell(3);
+        var p_invalid_pc = Math.round(report.invalid_primitives / report.total_primitives * 100);
+        var i = report.invalid_primitives + ' (' + p_invalid_pc + '%)';
+        td_i.appendChild(document.createTextNode(i));
+    }
+
+    body.appendChild(tbl);
+}
+
+idx_validation_summary();
+
+// Populate index.html with the validation results from the json report
+document.getElementById("in_file").innerHTML = report.input_file.split("/").pop();
+document.getElementById("version").innerHTML = report.val3dity_version;
+document.getElementById("time").innerHTML = report.time;
+document.getElementById("planarity_d2p_tol").innerHTML = report.planarity_d2p_tol;
+document.getElementById("planarity_n_tol").innerHTML = report.planarity_n_tol;
+document.getElementById("snap_tol").innerHTML = report.snap_tol;
+document.getElementById("overlap_tol").innerHTML = report.overlap_tol;
+
+document.write('<br>');
+error_overview();
