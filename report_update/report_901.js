b0b2adb23b8ae25941c7b70335fa191cba854a03
Balázs Dukai <balazs.dukai@gmail.com>
2018-02-12 12:16:56 +0100

    Add more test report.js; remove cityobject.html, primitives.html

---
+++ report_update/report_901.js
@@ -0,0 +1,26 @@
+var report = {
+    "errors_dataset": [{
+        "code": 901,
+        "description": "INVALID_INPUT_FILE",
+        "info": "Invalid GML structure, or that particular construction of GML is not supported yet. Please report at https://github.com/tudelft3d/val3dity/issues and provide the file.",
+        "type": "Error"
+    }],
+    "features": null,
+    "input_file": "./data/test_file_format/invalid_citygml_1.gml",
+    "invalid_features": 0,
+    "invalid_primitives": 0,
+    "overlap_tol": -1.0,
+    "overview_errors": [901],
+    "overview_features": null,
+    "overview_primitives": null,
+    "planarity_d2p_tol": 0.01,
+    "planarity_n_tol": 20.0,
+    "snap_tol": 0.001,
+    "time": "Mon Feb 12 10:59:30 2018 UTC",
+    "total_features": 0,
+    "total_primitives": 0,
+    "type": "val3dity report",
+    "val3dity_version": "2.0.4",
+    "valid_features": 0,
+    "valid_primitives": 0
+}
---
+++ report_update/report_valid_poly.js
@@ -0,0 +1,35 @@
+var report = {
+    "errors_dataset": null,
+    "features": [{
+        "errors_feature": null,
+        "id": "none",
+        "primitives": [{
+            "errors": null,
+            "id": "none",
+            "numberfaces": 6,
+            "numbershells": 1,
+            "numbervertices": 8,
+            "type": "Solid",
+            "validity": true
+        }],
+        "type": "GenericObject",
+        "validity": true
+    }],
+    "input_file": "./data/test_valid/basecube.poly",
+    "invalid_features": 0,
+    "invalid_primitives": 0,
+    "overlap_tol": -1.0,
+    "overview_errors": null,
+    "overview_features": ["GenericObject"],
+    "overview_primitives": ["Solid"],
+    "planarity_d2p_tol": 0.01,
+    "planarity_n_tol": 20.0,
+    "snap_tol": 0.001,
+    "time": "Mon Feb 12 11:07:32 2018 UTC",
+    "total_features": 1,
+    "total_primitives": 1,
+    "type": "val3dity report",
+    "val3dity_version": "2.0.4",
+    "valid_features": 1,
+    "valid_primitives": 1
+}
