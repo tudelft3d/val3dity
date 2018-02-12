c08941651c65a1c8281862d3b3d5677c20895710
Balázs Dukai <balazs.dukai@gmail.com>
2018-02-12 11:23:31 +0100

    Add test json reports

---
+++ report_update/report_GenericObject.js
@@ -0,0 +1,50 @@
+var report = {
+    "errors_dataset": null,
+    "features": [{
+        "errors_feature": null,
+        "id": "myid",
+        "primitives": [{
+            "errors": null,
+            "id": "elvisisalive",
+            "numbersolids": 2,
+            "primitives": [{
+                "errors": null,
+                "id": "0",
+                "numberfaces": 6,
+                "numbershells": 1,
+                "numbervertices": 8,
+                "type": "Solid",
+                "validity": true
+            }, {
+                "errors": null,
+                "id": "1",
+                "numberfaces": 6,
+                "numbershells": 1,
+                "numbervertices": 8,
+                "type": "Solid",
+                "validity": true
+            }],
+            "type": "CompositeSolid",
+            "validity": true
+        }],
+        "type": "GenericCityObject",
+        "validity": true
+    }],
+    "input_file": "../data/gml/csol1.gml",
+    "invalid_features": 0,
+    "invalid_primitives": 0,
+    "overlap_tol": -1.0,
+    "overview_errors": null,
+    "overview_features": ["GenericCityObject"],
+    "overview_primitives": ["CompositeSolid"],
+    "planarity_d2p_tol": 0.01,
+    "planarity_n_tol": 20.0,
+    "snap_tol": 0.001,
+    "time": "Fri Feb  9 17:26:03 2018 CET",
+    "total_features": 1,
+    "total_primitives": 1,
+    "type": "val3dity report",
+    "val3dity_version": "2.0.4",
+    "valid_features": 1,
+    "valid_primitives": 1
+}
