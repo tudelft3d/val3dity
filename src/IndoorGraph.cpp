65546bcbb0462d772af1a316485b47e19fb6e023
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-07-03 09:41:17 +0200

    skeleton IndoorGraph unit added

---
+++ src/IndoorGraph.cpp
@@ -0,0 +1,83 @@
+/*
+  val3dity 
+
+  Copyright (c) 2011-2018, 3D geoinformation research group, TU Delft  
+
+  This file is part of val3dity.
+
+  val3dity is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  val3dity is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with val3dity.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of val3dity, contact
+  Hugo Ledoux
+  <h.ledoux@tudelft.nl>
+  Faculty of Architecture & the Built Environment
+  Delft University of Technology
+  Julianalaan 134, Delft 2628BL, the Netherlands
+*/
+
+
+#include "IndoorGraph.h"
+#include "definitions.h"
+#include "input.h"
+
+namespace val3dity
+{
+
+IndoorGraph::IndoorGraph(std::string theid)
+{
+  if (theid == "")
+    _id = "MISSING_ID";
+  else
+    _id = theid;
+  _is_valid = -1;
+}
+
+
+IndoorGraph::~IndoorGraph()
+{}
+
+
+void IndoorGraph::add_vertex(std::string theid, double x, double y, double z, std::string dual, std::vector<std::string> connects)
+{
+  // std::map<std::string, std::tuple<Point3,std::string,std::vector<std::string>>> _vertices;
+  Point3 v(x, y, z);
+  _vertices[theid] = std::make_tuple(v, dual, connects);
+}
+
+bool IndoorGraph::validate() 
+{
+  std::cout << "VALIDATE IndoorGraph" << std::endl;
+  return true;
+  // if (_is_valid != -1)
+  //   return _is_valid;
+  // bool bValid = Feature::validate_generic(tol_planarity_d2p, tol_planarity_normals, tol_overlap);
+  // _is_valid = bValid;
+  // return bValid;
+}
+
+
+bool IndoorGraph::is_valid() {
+  if (_is_valid == 1) 
+    return true;
+  else
+    return false;
+}
+
+
+std::string IndoorGraph::get_type() {
+  return "IndoorGraph";
+}
+
+
+} // namespace val3dity
---
+++ src/IndoorGraph.h
@@ -0,0 +1,66 @@
+/*
+  val3dity 
+
+  Copyright (c) 2011-2018, 3D geoinformation research group, TU Delft  
+
+  This file is part of val3dity.
+
+  val3dity is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  val3dity is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with val3dity.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of val3dity, contact
+  Hugo Ledoux
+  <h.ledoux@tudelft.nl>
+  Faculty of Architecture & the Built Environment
+  Delft University of Technology
+  Julianalaan 134, Delft 2628BL, the Netherlands
+*/
+
+#ifndef __val3dity__IndoorGraph__
+#define __val3dity__IndoorGraph__
+
+#include "Feature.h"
+#include "definitions.h"
+
+
+namespace val3dity
+{
+
+class IndoorGraph
+{
+public:
+  IndoorGraph(std::string theid = "");
+  ~IndoorGraph();
+
+  void            add_vertex(std::string theid, 
+                             double x, 
+                             double y, 
+                             double z, 
+                             std::string dual, 
+                             std::vector<std::string> connects);
+  
+  bool            validate();
+  bool            is_valid();
+  std::string     get_type();
+
+protected:
+  std::string             _id;
+  int                     _is_valid; 
+  
+  std::map<std::string, std::tuple<Point3,std::string,std::vector<std::string>>> _vertices;
+
+};
+
+} // namespace val3dity
+
+#endif /* defined(__val3dity__IndoorGraph__) */
