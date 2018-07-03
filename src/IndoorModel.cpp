87a7f6f2265675e79ebc3d89e9a4fedaecc7b9b8
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-07-03 11:00:08 +0200

    skeleton unit IndoorModel added

---
+++ src/IndoorModel.cpp
@@ -0,0 +1,73 @@
+/*
+  val3dity 
+
+  Copyright (c) 2011-2018, 3D geoinformation research group, TU Delft  
+
+  This file is part of val3dity.
+
+  val3dity is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  val3dity is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with val3dity.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of val3dity, contact
+  Hugo Ledoux
+  <h.ledoux@tudelft.nl>
+  Faculty of Architecture & the Built Environment
+  Delft University of Technology
+  Julianalaan 134, Delft 2628BL, the Netherlands
+*/
+
+
+#include "IndoorModel.h"
+#include "definitions.h"
+#include "input.h"
+
+namespace val3dity
+{
+
+IndoorModel::IndoorModel(std::string theid)
+{
+  _id = theid;
+  _type = "IndoorModel"; 
+  _is_valid = -1;
+}
+
+
+IndoorModel::~IndoorModel()
+{}
+
+
+bool IndoorModel::validate(double tol_planarity_d2p, double tol_planarity_normals, double tol_overlap) 
+{
+  return true;
+  // if (_is_valid != -1)
+  //   return _is_valid;
+  // bool bValid = Feature::validate_generic(tol_planarity_d2p, tol_planarity_normals, tol_overlap);
+  // _is_valid = bValid;
+  // return bValid;
+}
+
+
+bool IndoorModel::is_valid() {
+  if (_is_valid == 1) 
+    return true;
+  else
+    return false;
+}
+
+
+std::string IndoorModel::get_type() {
+  return _type;
+}
+
+
+} // namespace val3dity
---
+++ src/IndoorModel.h
@@ -0,0 +1,59 @@
+/*
+  val3dity 
+
+  Copyright (c) 2011-2018, 3D geoinformation research group, TU Delft  
+
+  This file is part of val3dity.
+
+  val3dity is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  val3dity is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with val3dity.  If not, see <http://www.gnu.org/licenses/>.
+
+  For any information or further details about the use of val3dity, contact
+  Hugo Ledoux
+  <h.ledoux@tudelft.nl>
+  Faculty of Architecture & the Built Environment
+  Delft University of Technology
+  Julianalaan 134, Delft 2628BL, the Netherlands
+*/
+
+#ifndef __val3dity__IndoorModel__
+#define __val3dity__IndoorModel__
+
+#include "Feature.h"
+#include "definitions.h"
+#include "IndoorCell.h"
+#include "IndoorGraph.h"
+
+
+namespace val3dity
+{
+
+class IndoorModel : public Feature
+{
+public:
+  IndoorModel(std::string theid);
+  ~IndoorModel();
+  
+  bool            validate(double tol_planarity_d2p, double tol_planarity_normals, double tol_overlap = -1);
+  bool            is_valid();
+  std::string     get_type();
+
+protected:
+  std::map<std::string, IndoorCell*> _cells;
+  std::vector<IndoorGraph*>_graphs;
+
+};
+
+} // namespace val3dity
+
+#endif /* defined(__val3dity__IndoorModel__) */
