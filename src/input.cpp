d6586f98a5517adda7cd78f95262fbe0bfd616c4
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-04-30 14:42:49 +0200

    Fix for issue #140

--- src/input.cpp
+++ src/input.cpp
@@ -549,8 +549,10 @@ void get_namespaces(pugi::xml_node& root, std::map<std::string, std::string>& ns
         sns = "citygml";
         vcitygml = "v2.0";
       }
-      else if (value.find("http://www.opengis.net/gml") != std::string::npos)
-        sns = "gml";
+      else if ( (value.find("http://www.opengis.net/gml") != std::string::npos) &&
+                (value.find("http://www.opengis.net/gmlcov") == std::string::npos) ) {
+          sns = "gml";
+      }
       else if (value.find("http://www.opengis.net/citygml/building") != std::string::npos)
         sns = "building";
       else if (value.find("http://www.w3.org/1999/xlink") != std::string::npos)
@@ -983,8 +985,10 @@ void get_namespaces(pugi::xml_node& root, std::string& vcitygml) {
         sns = "citygml";
         vcitygml = "v2.0";
       }
-      else if (value.find("http://www.opengis.net/gml") != std::string::npos)
-        sns = "gml";
+      else if ( (value.find("http://www.opengis.net/gml") != std::string::npos) &&
+                (value.find("http://www.opengis.net/gmlcov") == std::string::npos) ) {
+          sns = "gml";
+      }
       else if (value.find("http://www.opengis.net/citygml/building") != std::string::npos)
         sns = "building";
       else if (value.find("http://www.opengis.net/citygml/relief") != std::string::npos)
