2ace3ee18a1eb9ac6f8f8a9464b1e95d74df7bad
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-08-06 16:09:32 +0200

    Add demo how to use val3dity as a library, with main.cpp and cmake file

---
+++ demo_lib/main.cpp
@@ -0,0 +1,47 @@
+
+#include "val3dity.h" 
+#include "nlohmann-json/json.hpp"
+
+#include <fstream>
+
+using json = nlohmann::json;
+
+int main(int argc, char *argv[])
+{
+  std::streambuf* clog_buf = std::clog.rdbuf();
+  std::clog.rdbuf(NULL);
+  std::streambuf* cout_buf = std::cout.rdbuf();
+  std::cout.rdbuf(NULL);
+  
+
+  std::ifstream input("/Users/hugo/data/cityjson/misc/cube.json");
+  // std::ifstream input("/Users/hugo/data/cityjson/examples/delft/v100/a2.json");
+  json j;
+  try 
+  {
+    input >> j;
+  }
+  catch (nlohmann::detail::parse_error e) 
+  {
+    std::cerr << "Input file not a valid JSON file." << std::endl;
+    return 0;
+  }
+  if (j["type"] != "CityJSON") {
+    std::cerr << "Input file not a valid JSON file." << std::endl;
+    return 0;  
+  }
+
+  bool re = val3dity::validate_cityjson(j, 0.001);
+
+  // restore buffer
+  std::clog.rdbuf(clog_buf);
+  std::cout.rdbuf(cout_buf);
+
+  if (re == true)
+    std::cout << "VALID" << std::endl;
+  else
+    std::cout << "INVALID :(" << std::endl;
+
+  
+  return 0;
+}
