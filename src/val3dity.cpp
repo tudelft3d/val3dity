236b1eaac9e30e83e5e384c6b42ae28f864a15a4
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-08-06 15:05:46 +0200

    WIP to create a val3dity library

---
+++ src/val3dity.cpp
@@ -0,0 +1,11 @@
+#include "val3dity.h"
+#include <iostream>
+
+namespace val3dity
+{
+
+bool validate(json& fs) {
+  return true;
+}
+
+}
---
+++ src/val3dity.h
@@ -0,0 +1,11 @@
+#include "nlohmann-json/json.hpp"
+
+using json = nlohmann::json;
+
+namespace val3dity
+{
+
+  bool validate(json& features);
+
+}
+
