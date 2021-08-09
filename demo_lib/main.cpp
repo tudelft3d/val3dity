5bcf3cf6f2ff29cc8f0a63e9ad992322b3c55e39
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-08-09 16:49:58 +0200

    WIP of example main.cpp for a library

--- demo_lib/main.cpp
+++ demo_lib/main.cpp
@@ -8,14 +8,9 @@ using json = nlohmann::json;
 
 int main(int argc, char *argv[])
 {
-  std::streambuf* clog_buf = std::clog.rdbuf();
-  std::clog.rdbuf(NULL);
-  std::streambuf* cout_buf = std::cout.rdbuf();
-  std::cout.rdbuf(NULL);
-  
 
-  std::ifstream input("/Users/hugo/data/tu3djson/cube.json");
-  // std::ifstream input("/Users/hugo/data/cityjson/misc/cube.json");
+  // std::ifstream input("/Users/hugo/data/tu3djson/cube.json");
+  std::ifstream input("/Users/hugo/data/cityjson/misc/cube.json");
   // std::ifstream input("/Users/hugo/data/cityjson/examples/delft/v100/a2.json");
   json j;
   try 
@@ -27,22 +22,26 @@ int main(int argc, char *argv[])
     std::cerr << "Input file not a valid JSON file." << std::endl;
     return 0;
   }
+
   // if (j["type"] != "CityJSON") {
   //   std::cerr << "Input file not a valid JSON file." << std::endl;
   //   return 0;  
   // }
   
+  json jr;
+  // bool re = val3dity::validate_one_geom(j["features"][0]["geometry"], jr);
+  // std::cout << jr << std::endl;
 
-  bool re = val3dity::validate_one_geom(j["features"][0]["geometry"]);
+  // bool re = val3dity::validate_one_geom(j["features"][0]["geometry"]);
   // std::vector<bool> re = val3dity::validate_tu3djson(j);
-  // bool re = val3dity::validate_cityjson(j, 0.001);
-
-  // restore buffer
-  std::clog.rdbuf(clog_buf);
-  std::cout.rdbuf(cout_buf);
+  // std::vector<bool> re = val3dity::validate_tu3djson(j, jr);
+  // std::cout << jr << std::endl;
+  // std::ofstream o("/Users/hugo/temp/r.json");
+  // o << jr.dump(2) << std::endl;
+  // bool re = val3dity::validate_cityjson(j);
+  bool re = val3dity::validate_cityjson(j, jr);
 
   // std::for_each(re.begin(), re.end(), [](const bool n) { std::cout << n << std::endl; });
-
   if (re == true)
     std::cout << "VALID" << std::endl;
   else
