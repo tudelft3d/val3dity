64f9ae5b3c780baf090bdff0b87045e5dad6df1f
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-08-09 14:12:40 +0200

    version to 2.3 beta

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ using namespace std;
 using namespace val3dity;
 using json = nlohmann::json;
 
-std::string VAL3DITY_VERSION = "2.2.0";
+std::string VAL3DITY_VERSION = "2.3.0-beta.1";
 
 
 std::string print_summary_validation(std::vector<Feature*>& lsFeatures, IOErrors& ioerrs);
