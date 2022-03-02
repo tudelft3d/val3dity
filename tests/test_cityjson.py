a02d625d9eb8ec4353535e8e127dd317f2f8534d
Hugo Ledoux <h.ledoux@tudelft.nl>
2022-03-02 15:58:45 +0100

    Add the CityJSON equivalent of the CityGML files with complex geom cases
    like 2 adjacent cubes overlapping

--- tests/test_cityjson.py
+++ tests/test_cityjson.py
@@ -24,6 +24,15 @@ def data_cj_iv_gt(request, dir_cityjson):
             request.param))
     return([file_path])
 
+@pytest.fixture(scope="module",
+                params=["torus.city.json"])
+def data_cj_v11(request, dir_cityjson):
+    file_path = os.path.abspath(
+        os.path.join(
+            dir_cityjson,
+            request.param))
+    return([file_path])
+
 #----------------------------------------------------------------------- Tests
 def test_valid_geomtemplates(validate, data_cj_v_gt, unittests):
     error = validate(data_cj_v_gt, options=unittests)
@@ -32,3 +41,8 @@ def test_valid_geomtemplates(validate, data_cj_v_gt, unittests):
 def test_invalid_geomtemplates(validate, data_cj_iv_gt, unittests):
     error = validate(data_cj_iv_gt, options=unittests)
     assert(error == [203])
+
+
+def test_cityjson_v11(validate, data_cj_v11, unittests):
+    error = validate(data_cj_v11, options=unittests)
+    assert(error == [])
