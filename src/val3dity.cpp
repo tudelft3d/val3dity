ab563b65dd94903edd5f0360b7ec5323c511825c
Hugo Ledoux <h.ledoux@tudelft.nl>
2022-03-07 13:36:30 +0100

    Bump to (final?) beta version for v2.3.0

--- src/val3dity.cpp
+++ src/val3dity.cpp
@@ -38,7 +38,7 @@
 namespace val3dity
 {
 
-std::string VAL3DITY_VERSION = "2.3.0b3";
+std::string VAL3DITY_VERSION = "2.3.0b4";
 
 void 
 validate_no_coutclog(std::vector<Feature*>& lsFeatures,                        
