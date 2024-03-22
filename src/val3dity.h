5fd680cdd9e9c360efbfa12922bb0d7ebfd4b054
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-03-22 10:29:25 +0100

    Add draft of new library function to validate std::vectors (à la OBJ)

--- src/val3dity.h
+++ src/val3dity.h
@@ -65,5 +65,21 @@ validate(std::string& input,
          double planarity_n_tol=20.0, 
          double overlap_tol=-1.0);
 
+json 
+validate(const std::vector<std::vector<double>>& vertices,
+         const std::vector<std::vector<int>>& triangle_ids,
+         double tol_snap=0.001, 
+         double planarity_d2p_tol=0.01, 
+         double planarity_n_tol=20.0, 
+         double overlap_tol=-1.0);
+
+// json 
+// validate(std::string& input,
+//          std::string format, 
+//          double tol_snap=0.001, 
+//          double planarity_d2p_tol=0.01, 
+//          double planarity_n_tol=20.0, 
+//          double overlap_tol=-1.0);
+
 
 }
