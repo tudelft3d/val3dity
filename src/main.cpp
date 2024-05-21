86114ed0c752c43b1aafd67212c16603cf38442a
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-05-21 10:59:12 +0200

    Bump the beta version for v2.5

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ using namespace std;
 using namespace val3dity;
 using json = nlohmann::json;
 
-std::string VAL3DITY_VERSION = "2.5.0b0";
+std::string VAL3DITY_VERSION = "2.5.0b1";
 
 
 std::string print_summary_validation(std::vector<Feature*>& lsFeatures, IOErrors& ioerrs);
--- src/val3dity.cpp
+++ src/val3dity.cpp
@@ -41,7 +41,7 @@
 namespace val3dity
 {
 
-std::string VAL3DITY_VERSION = "2.5.0b0";
+std::string VAL3DITY_VERSION = "2.5.0b1";
 
 struct verror : std::exception {
   std::string whattext;
